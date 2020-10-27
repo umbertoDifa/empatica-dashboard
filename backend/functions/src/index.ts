72872f77e4f17fbc1589018626ebe2b470b7b13e
Umberto Di Fabrizio <umberto.difabrizio@boomimagestudio.com>
2020-10-28 00:00:50 +0100

    ED-24 added tests

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,54 +1,6 @@
 import * as functions from 'firebase-functions';
-import moment from 'moment';
-
-const MOCK_COUNTRIES = [
-  'Kazakhstan',
-  'Philippines',
-  'China',
-  'Russia',
-  'Philippines',
-  'Czech Republic',
-  'Georgia',
-  'Netherlands',
-  'Greece',
-  'Poland',
-  'China',
-  'Poland',
-  'Japan',
-  'Yemen',
-  'Macedonia',
-  'Brazil',
-  'Poland',
-  'Thailand',
-  'Sweden',
-  'Russia',
-  'Brazil',
-  'Panama',
-  'Dominican Republic',
-  'China',
-  'Sweden',
-  'Sweden',
-  'China',
-  'Indonesia',
-  'Peru',
-  'Indonesia',
-  'Ukraine',
-  'Indonesia',
-  'Chile',
-  'Mexico',
-];
+import { augmentData } from './helpers';
 
 export const onDataPointAdded = functions.database
   .ref('/downloads/{download_id}')
-  .onCreate((snapshot, context) => {
-    const id = context.params.download_id;
-    const val = snapshot.val();
-    const country = mockCountry(); // Outbound traffic not allowed on free plan, mocking call
-    console.log(id, val, country);
-    return snapshot.ref.set({ ...val, downloaded_at: moment().unix(), country: country });
-  });
-
-function mockCountry() {
-  const index = Math.round(Math.random() * (MOCK_COUNTRIES.length - 1));
-  return MOCK_COUNTRIES[index];
-}
+  .onCreate(augmentData);
